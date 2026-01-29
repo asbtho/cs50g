3bfe1b376b0e0d80df44f742552d54f53bc1d47e
asbtho <a_thom89@hotmail.com>
2026-01-29 20:12:27 +0100

    week11 portal assignment complete

---
+++ assignments/portal/Assets/Scripts/EndGame.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class EndGame : MonoBehaviour {
+
+	public Text WinText;
+	public AudioSource WinAudio;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	void Awake() {
+		WinText = GameObject.Find("WinningText").GetComponent<Text>();
+		WinAudio = GameObject.Find("WinningSound").GetComponent<AudioSource>();
+	}
+
+	void OnTriggerEnter(){
+		WinText.enabled = true;
+		WinAudio.Play(0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
